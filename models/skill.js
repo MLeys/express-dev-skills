0515ada7803cfca5432c5bb7434caa74d2edd64d
username <mleyspainting@gmail.com>
2022-11-23 19:49:43 -0500

    Completed first part of express routes. Next Step: separating out controllers from routes.

---
+++ models/skill.js
@@ -0,0 +1,24 @@
+const skills = [
+    {id: 100001, skill: 'Java', level: 'beginner' },
+    {id: 100002, skill: 'JavaScript', level: 'beginner' },
+    {id: 100003, skill: 'HTML', level: 'beginner' },
+    {id: 100004, skill: 'CSS', level: 'beginner' },
+    {id: 100005, skill: 'Python', level: 'beginner' },
+    {id: 100006, skill: 'Express', level: 'none' },
+    {id: 100007, skill: 'Ruby', level: 'none'},
+    {id: 100008, skill: 'Rails', level: 'none'}
+  ];
+  
+  module.exports = {
+    getAll: function() {
+        return skills
+    }
+  };
+
+
+
+//   function getAll() {
+//     return skills;
+
+//  }
+ 
